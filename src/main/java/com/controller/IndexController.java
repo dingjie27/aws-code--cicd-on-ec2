b585b1545494f5c67ee3d6093cefbc9e89ef32b0
Ding <jieding@f018984ead2d.ant.amazon.com>
2019-03-21 11:05:07 +0800

    newadd

---
+++ src/main/java/com/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @Value("${server.port}")
+    String port;
+    @Autowired
+    com.utils.ElasticacheTool elasticacheTool;
+
+    @GetMapping("/")
+    public String getIndex() {
+        System.out.println("server is up and the port is "+ port);
+        return "hello,this is a response from my web service!";
+    }
+
+    @GetMapping("/getkey")
+    public String getRedis() {
+        return elasticacheTool.getByKey();
+    }
+}
+
