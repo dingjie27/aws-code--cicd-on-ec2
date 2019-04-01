929e79b275f97dec39bb1a81b036438e86b2c7c7
dingjie27 <48076430+dingjie27@users.noreply.github.com>
2019-04-01 17:08:36 +0800

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "sdfafasfsfsaf" > a.log
 sudo su
-java -jar  /home/ec2-user/demo/demoforcicd-1.0-SNAPSHOT.jar & > test.log
+java -jar  /home/ec2-user/demoforcicd-1.0-SNAPSHOT.jar & > test.log
