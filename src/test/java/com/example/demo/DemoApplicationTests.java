31cd99bce5689f87eeb44dd36df143b225567e58
jieding <dingjie27@163.com>
2019-04-28 15:16:08 +0800

    asdfasdfasdf

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import com.aws.demo4cicf.DemoApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
 public class DemoApplicationTests {
 
     @Test
