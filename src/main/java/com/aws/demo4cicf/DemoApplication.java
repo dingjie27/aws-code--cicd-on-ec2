3d944b2438f317929c7a1a3187ee077a868ad485
dingjie27 <48076430+dingjie27@users.noreply.github.com>
2019-05-07 15:33:55 +0800

    Update DemoApplication.java

--- src/main/java/com/aws/demo4cicf/DemoApplication.java
+++ src/main/java/com/aws/demo4cicf/DemoApplication.java
@@ -12,6 +12,10 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("web server is up .....");
+        
+        
+        
+        
     }
 
 }
